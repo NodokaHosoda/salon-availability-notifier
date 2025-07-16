edbbc364743eb129c54c78fbf092816233876e10
Nodoka <nonno.wmyb1106@gmail.com>
2025-07-16 16:06:01 +0000

    とりあえずplaywrightは動くようになった

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from urllib.parse import urlparse, parse_qs
-import time, os, requests, asyncio
+import time, os, asyncio, re
 from datetime import datetime
-from playwright.async_api import async_playwright # type: ignore
+from playwright.async_api import async_playwright
 
 
 LINE_TOKEN = os.environ.get("LINE_TOKEN")
 URL = "https://beauty.hotpepper.jp/slnH000440848/"
+
+"""
+TODO: LINE通知の実装
 def send_line_notify(message):
     if not LINE_TOKEN:
         print("LINE_TOKEN is missing")
@@ -15,73 +18,94 @@ def send_line_notify(message):
         headers={"Authorization": f"Bearer {LINE_TOKEN}"},
         data={"message": message}
     )
+"""
 
 async def check_availability():
-    date = "20250923"
+    date = "20250814"
     request_date = datetime.strptime(date, "%Y%m%d")
     availability_elements = []
 
     async with async_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--no-sandbox"])
-        page = browser.new_page()
-        page.goto(URL, wait_until="domcontentloaded")
+        browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
+        page = await browser.new_page()
+        await page.goto(URL, wait_until="domcontentloaded")
 
         # メニューを選択画面へ
-        page.get_by_text("空席確認・予約する").click()
-        page.wait_for_load_state("load")
+        await page.get_by_text("空席確認・予約する").nth(0).click()
+        await page.wait_for_load_state("load")
 
         # メニューを選択し空席確認へ
-        tr = page.locator(f"p.couponMenuName", has_text="カットのみ").locator("xpath=ancestor::tr")
-        tr.locator("text=空席確認・予約する").click()
-        page.wait_for_load_state("load")
+        cut_only = page.locator(f"p.couponMenuName", has_text="カットのみ")
+        count = await cut_only.count()
+        # カットのみが含まれる要素が複数あるのでカットのみに完全一致する要素を探す
+        for i in range(count):
+            ele = cut_only.nth(i)
+            text = await ele.text_content()
+            if text.strip() == "カットのみ":
+                cut_only = ele
+                break
+
+        tr = cut_only.locator("xpath=ancestor::tr")
+        await tr.locator("text=空席確認・予約する").click()
+        await page.wait_for_load_state("load")
 
         while True:
             calendar = page.locator("table.innerTable")
-            curr_month = calendar.locator("th.monthCell").text_content()
             last_date_tr = calendar.locator("tr.dayCellContainer")
-            text = await last_date_tr.locator("th:last-of-type").text_content()
-            date = text.split("\n")[0]
-            date = curr_month.replace("年", "-").replace("月", "-") + date
-            last_date = datetime.strptime(date, "%Y-%m-%d")
+            last_date_obj = await last_date_tr.locator("th:last-of-type").text_content()
+            last_date = convert_to_ymd(last_date_obj)
             # カレンダーの空きを検出
             availability_elements += await calendar.locator("a.icnOpen").all()
 
-            if(last_date < request_date):
-                # 次の一週間へ
-                calendar.locator("a.arrowPagingWeekR").click()
-                page.wait_for_load_state("load")
+            if last_date < request_date:
+                next_btn = page.locator("a.arrowPagingWeekR")
+                if await next_btn.count() > 0 and await next_btn.is_enabled():
+                    await next_btn.click()
+                    await page.wait_for_load_state("load")
+                else:
+                    break
             else:
                 break
 
-        count = availability_elements.count()
+        count = len(availability_elements)
         if count > 0:
-            data = create_avaliable_date_list(availability_elements)
-            console.log(data)
+            data = await create_avaliable_date_list(availability_elements, request_date)
+            print(data)
+        else:
+            print("No available dates found.")
 
-        browser.close()
+        await browser.close()
 
-def create_avaliable_date_list(elements):
-    for ele in range(elements):
-        url = ele.get_attribute("href")
-        parsed_url = urlparse(href)
+async def create_avaliable_date_list(elements, request_date: datetime):
+    text = ""
+    for ele in elements:
+        url = await ele.get_attribute("href")
+        parsed_url = urlparse(url)
         params = parse_qs(parsed_url.query)
         date = params.get("rsvRequestDate1", [""])[0]
-        time = params.get("rsvRequestTime1", [""])[0]
-        date = format_date(date)
-        time = format_time(time)
-        text += f"{date} {time}\n"
+
+        if datetime.strptime(date, "%Y%m%d") <= request_date:
+            time = params.get("rsvRequestTime1", [""])[0]
+            date = format_date(date)
+            time = format_time(time)
+            text += f"{date} {time}\n"
     return text
 
-def format_date(str: date):
+def format_date(date_str: str):
     jp_weekdays = ["月", "火", "水", "木", "金", "土", "日"]
-    date_obj = datetime.strptime(date, "%Y%m%d")
+    date_obj = datetime.strptime(date_str, "%Y%m%d")
     weekday_jp = jp_weekdays[date_obj.weekday()]
     return date_obj.strftime(f"%Y年%m月%d日（{weekday_jp}）")
 
-def format_time(str: time):
-    time_obj = datetime.strptime(time, "%H%M")
+def format_time(time_str: str):
+    time_obj = datetime.strptime(time_str, "%H%M")
     return time_obj.strftime("%H:%M") 
 
-if __name__ == "__main__":
-    check_availability()
+def convert_to_ymd(date_str: str) -> datetime:
+    # （水）など日本語の曜日部分を削除
+    cleaned = re.sub(r"[（(].*?[)）]", "", date_str)
+    # 日付文字列を datetime オブジェクトに変換
+    return datetime.strptime(cleaned.strip(), "%a %b %d %H:%M:%S JST %Y")
 
+if __name__ == "__main__":
+    asyncio.run(check_availability())
