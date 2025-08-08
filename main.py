7be42dd4a16ba24836851bd69a0100ee491a7793
Nodoka <nonno.wmyb1106@gmail.com>
2025-08-08 20:29:49 +0900

    test webhook

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 
 app = Flask(__name__)
 
+# 環境変数からLINEの設定を取得
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# Webhook受信用
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# メッセージ受信イベント
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    reply_text = f"あなたが送ったメッセージ: {event.message.text}"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
+
 @app.route("/")
 def index():
-    return "Hello from Cloud Run + Flask!"
+    return "LINE Bot is running on Cloud Run!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
